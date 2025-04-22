f08000940d5debbe9905478bfa6f154f7b24a04c
muhlisol <muhlisolzy@gmail.com>
2025-04-22 04:48:26 -0100

    seed user

--- seeders/20250422050416-user.js
+++ seeders/20250422050416-user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { hashPassword } = require('../helpers/bcrypt');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
